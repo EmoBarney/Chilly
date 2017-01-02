9a173a5c3079e274c32ac1f5cb5a752e3d7b7677
Davis Cho <davischo@Daviss-MacBook-Pro-2.local>
2017-01-02 14:52:38 -0800

    Initialization

---
+++ www/js/routes.js
@@ -0,0 +1,65 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.owned', {
+    url: '/owned',
+    views: {
+      'tab4': {
+        templateUrl: 'templates/owned.html',
+        controller: 'ownedCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.expirations', {
+    url: '/expirations',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/expirations.html',
+        controller: 'expirationsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.groceryList', {
+    url: '/tobuy',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/groceryList.html',
+        controller: 'groceryListCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'templates/signup.html',
+    controller: 'signupCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
