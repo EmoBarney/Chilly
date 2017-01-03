4d5565253484401b2ef28caf505283a246864232
EmoBarney <chodavis@gmail.com>
2017-01-03 02:03:12 -0800

    Fully implemented signin and signout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,29 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ionic','app.services'])
   
-.controller('ownedCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('ownedCtrl', ['$scope', '$stateParams',
+// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
-
+function ($scope, $stateParams ) {
 }])
    
-.controller('expirationsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('expirationsCtrl', ['$scope', '$stateParams', '$state', 'loginService',
+// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $state, loginService) {
 
+	loginService.loginStatusChanged( function(user){
+		if( user ){
+		}
+		else{
+			$state.go( 'login', {updateRequired:true} );
+		}
+	});
+
+	$scope.signout = function(){
+		loginService.signout();
+	};
 
 }])
    
@@ -24,11 +35,50 @@ function ($scope, $stateParams) {
 
 }])
       
-.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('loginCtrl', ['$scope', '$stateParams',
+'$ionicPopup','loginService', '$state',
+ // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicPopup, loginService, $state) {
 
+	$scope.loginForm = {};
+
+	loginService.loginStatusChanged(function(user){
+		if( user ){
+			console.log('Logged in!');
+			$state.go('tabsController.owned', {updateRequired:true});
+		}
+
+	});
+
+	$scope.login = function(){
+		var email = $scope.loginForm.email;
+		var password = $scope.loginForm.password;
+		var success = false;
+
+		if( email && password ){
+			loginService
+				.login( email, password )
+				.catch( function(error){
+					$ionicPopup.alert({
+						title: 'Login Failed',
+						template: 'Wrong email or password, NOOB' 
+					})
+				});
+		}
+		else{
+			$ionicPopup.alert({
+				title:'Invalid Input',
+				template: 'Please enter email and password'
+			});
+		}
+
+		
+		console.log('email entered: ' + email);
+		console.log('password entered: ' + password);
+		
+	};
 
 }])
    
